64a55758e4517a1cd148c42713671d0a872010bf
nkdroidsolutions <nkdroidsolutions@gmail.com>
2017-01-13 13:47:27 +0530

    Initial commit

---
+++ app/src/main/java/com/nkdroid/listview/demo/DummyClass.java
@@ -0,0 +1,14 @@
+package com.nkdroid.listview.demo;
+
+/**
+ * Created by Surabhi on 1/9/2017.
+ */
+
+public class DummyClass {
+
+    private String username;
+
+    private String password;
+
+
+}
